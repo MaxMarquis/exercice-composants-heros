dd9cb6b83770e51fbd90457c0f75274bdd54e964
Max Marquis <maxim.marquis@gmail.com>
2021-10-17 20:21:31 -0400

    exercice 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
+import { EnteteComponent } from './components/entete/entete.component';
+import { RechercheComponent } from './components/recherche/recherche.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { PiedPageComponent } from './components/pied-page/pied-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    EnteteComponent,
+    RechercheComponent,
+    CarouselComponent,
+    HeroComponent,
+    PiedPageComponent
   ],
   imports: [
     BrowserModule
